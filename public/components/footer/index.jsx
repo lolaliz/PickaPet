f41d0ae5a223c1bd0d9baa3bc5dda1862440165c
Daniel <danstraw86@gmail.com>
2018-05-17 18:29:51 -0700

    created header and footer components, added yelp object to keys.js

---
+++ public/components/footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class Footer extends React.Component {
+    render() {
+        return (
+            <footer id="foot">PickaPet © 2018 by Janet Hwu, Elizabeth Martinez, Andres Martinez, & Daniel Strawbridge</footer>
+        );
+    }
+}
--- public/components/header/index.jsx
+++ public/components/header/index.jsx
@@ -1,1 +1,15 @@
 // build header component here
+import React from 'react';
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <header id="head">
+                <h2>PickAPet</h2>
+                <a href="/">Retake Survey</a> |
+                <a href="https://github.com/jkhwu/PickaPet">GitHub</a> |
+                <button type="submit" value="log out">Log Out</button>
+            </header>
+        );
+    }
+}
